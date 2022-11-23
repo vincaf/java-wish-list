40a5c8842b08fac6657dc2cdf2d9f7d2012a769c
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-23 15:31:55 +0100

    feat: add StringComparator static class and compare method

--- Wish List/src/org/generation/italy/christmas/Main.java
+++ Wish List/src/org/generation/italy/christmas/Main.java
@@ -1,6 +1,7 @@
 package org.generation.italy.christmas;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -28,8 +29,36 @@ public class Main {
 		
 		wishList.sort(String.CASE_INSENSITIVE_ORDER);
 		System.out.println("\n----------\n");
-		System.out.println("Wish list: " + wishList);
+		System.out.println("Wish list in alphabetical order: " + wishList);
+		
+		wishList.sort(new StringComparator());
+		System.out.println("Wish list sorted by vowels: " + wishList);
 		
 	}
+	
+	public static class StringComparator implements Comparator<String> {
 
+		@Override
+		public int compare(String o1, String o2) {
+			int v1 = 0;
+			int v2 = 0;
+
+			for (int i = 0; i < o1.length(); i++) {
+				if (o1.charAt(i) == 'a' || o1.charAt(i) == 'e' || o1.charAt(i) == 'i' || o1.charAt(i) == 'o'
+						|| o1.charAt(i) == 'u') {
+					v1++;
+				}
+			}
+
+			for (int i = 0; i < o2.length(); i++) {
+				if (o2.charAt(i) == 'a' || o2.charAt(i) == 'e' || o2.charAt(i) == 'i' || o2.charAt(i) == 'o'
+						|| o2.charAt(i) == 'u') {
+					v2++;
+				}
+			}
+
+			return v2 - v1;
+		}
+
+	}
 }
